2a8cded1a3b9a7c4f2e43c94b3cb85df87978c2a
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-06-13 22:48:47 -0300

    segundo commit

--- src/SignIn/index.tsx
+++ src/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput, ImageBackground } from "react-native";
 import { FontAwesome } from '@expo/vector-icons'
+import LinearGradient from "react-native-linear-gradient";
 
 export default function SignIn() {
     return(
@@ -10,11 +11,11 @@ export default function SignIn() {
             </View>
 
             <View style={styles.saluContainer}>
-                <Text style={styles.saluText}>Salu</Text>
+                <Text style={styles.saluText}>Salud</Text>
             </View>
 
             <View>
-                <Text style={styles.signinText}>Faça login na sua conta</Text>
+                <Text style={styles.signinText}>Acesse seus EXAMES</Text>
             </View>
 
             <View style={styles.inputContainer}>
@@ -35,6 +36,17 @@ export default function SignIn() {
                 <Text style={styles.trocarSenhaInput}>Esqueceu a senha ?</Text>
             </View>
 
+            <View style={styles.signInButtonContainer}>
+            <Text style={styles.signIn}>Acessar</Text>
+            <View style={styles.signInIcon}>
+                <FontAwesome name={'arrow-right'} size={24} />
+            </View>
+            </View>
+
+            <View style={styles.leftVectorContainer}>
+            <ImageBackground  source={require('../assets/vector2.png')} style={styles.leftVectorImage} />
+            </View>
+
         </View>
     )
 }
@@ -44,6 +56,7 @@ const styles = StyleSheet.create({
     container: {
         flex:1,
         backgroundColor: '#f5f5f5',
+        position: 'relative'
     },
 
 
@@ -91,7 +104,40 @@ const styles = StyleSheet.create({
         textAlign: 'right',
         width: '90%',
         fontSize: 15,
+    },
+
+    signInButtonContainer: {
+    flexDirection: 'row',
+    marginTop: 70,
+    justifyContent: 'flex-end',
+    width: '90%'
+    },
+
+    signIn: {
+        color: '#262626',
+        fontSize: 25,
+        fontWeight: 'bold'
+    },
+
+    signInIcon: {
+        height: 36,
+        width: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 5
+    },
+
+    leftVectorContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+    },
+
+    leftVectorImage: {
+        height:250,
+        width: 170
     }
+
 
    
 })
